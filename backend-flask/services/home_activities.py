c918c51411c17b96ad403555760ceec77d6ba44e
Abimbola93 <beemborsugar@yahoo.com>
2023-03-22 04:46:00 +0000

    backend check for token

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,13 +5,13 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
+  def run(cognito_user_id=None):
     #Logger.info(HomeActivities)
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
-      now = datetime.now(timezone.utc).astimezone()
+
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -42,7 +42,7 @@ class HomeActivities:
         'replies': []
       },
       {
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'uuid': '3f108802-461f-4c72-bba1-117c2c009b49',
         'handle':  'Garek',
         'message': 'My dear doctor, I am just simple tailor',
         'created_at': (now - timedelta(hours=1)).isoformat(),
@@ -51,5 +51,18 @@ class HomeActivities:
         'replies': []
       }
       ]
+
+    if cognito_user_id != None:
+      extra_crud = {
+          'uuid': '3f108802-461f-4c72-bba1-117c2c009b49',
+          'handle':  'Lore',
+          'message': 'My dear brother, it the humans that are the problem',
+          'created_at': (now - timedelta(hours=1)).isoformat(),
+          'expires_at': (now + timedelta(hours=12)).isoformat(),
+          'likes': 1042,
+          'replies': []
+          }
+        results.insert(0,extra_crud)
+
       span.set_attribute("app.result_length", len(results))
       return results
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-    model = {
+   try
+     model = {
       'errors': None,
       'data': None
     }
@@ -20,4 +22,16 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+
+      subsegment = xray_recorder.begin_subsegment('mock-data')
+      # xray ---
+      dict = {
+        "now": now.isoformat(),
+        "results-size": len(model['data'])
+      }
+      subsegment.put_metadata('key', dict, 'namespace')
+      xray_recorder.end_subsegment()
+    finally:  
+    #  # Close the segment
+      xray_recorder.end_subsegment()
     return model
